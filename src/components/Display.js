cda34efe2ae55178ee6ff58d62a7c552b9cff7c5
George <71434803+george-swift@users.noreply.github.com>
2021-06-23 12:10:24 +0100

    Add tests for display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
   <div className="col-12 h-25 text-white">
-    <div className="problem">10 x 0</div>
+    <div className="problem" />
     <div className="result">
       {result}
     </div>
