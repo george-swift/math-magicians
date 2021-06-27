5da86ea915e478e011824e140b9478e4cc0ea312
George <71434803+george-swift@users.noreply.github.com>
2021-06-27 14:30:58 +0100

    Add the calculate module to handle data and button name

---
+++ src/logic/calculate.js
@@ -0,0 +1,138 @@
+import operate from './operate';
+
+export default function calculate(data, buttonName) {
+  const {
+    total,
+    next,
+    operation,
+    temp,
+  } = data;
+
+  switch (buttonName) {
+    case 'AC':
+      return {
+        total: null,
+        next: null,
+        operation: null,
+        temp: false,
+      };
+
+    case '+/-':
+      return next
+        ? {
+          total,
+          next: `${next * -1}`,
+          operation,
+          temp: false,
+        }
+        : {
+          total: `${total * -1}`,
+          next,
+          operation: null,
+          temp: false,
+        };
+
+    case '%':
+      if (next) {
+        return {
+          total,
+          next: next.includes('%') ? next : `${next / 100}`,
+          operation,
+          temp: false,
+        };
+      }
+
+      return total
+        ? {
+          total: total.includes('%') ? total : `${total / 100}`,
+          next,
+          operation,
+          temp: false,
+        }
+        : {
+          total: '0',
+          next,
+          operation,
+        };
+
+    case '+':
+    case '-':
+    case 'x':
+    case '/':
+      return (next && operation)
+        ? {
+          total: operate(total, operation, next),
+          next: null,
+          operation: buttonName,
+          temp: false,
+        }
+        : {
+          total,
+          next: null,
+          operation: buttonName,
+          temp: false,
+        };
+
+    case '.':
+      if (next) {
+        return {
+          total,
+          next: next.includes('.') ? next : `${next}.`,
+          operation,
+          temp: false,
+        };
+      }
+      if (total) {
+        return {
+          total: total.includes('.') ? total : `${total}.`,
+          next,
+          operation,
+          temp: false,
+        };
+      }
+      return {
+        total: '0.',
+        next,
+        operation,
+      };
+
+    case '=':
+      return operation
+        ? {
+          total: operate(total, operation, next),
+          next: null,
+          operation: null,
+          temp: true,
+        }
+        : {
+          total,
+          next: null,
+          operation: null,
+          temp: true,
+        };
+
+    default:
+      if (operation) {
+        return {
+          total,
+          next: next ? `${next}${buttonName}` : buttonName,
+          operation,
+        };
+      }
+
+      if (temp) {
+        return {
+          total: buttonName,
+          next,
+          operation,
+          temp: false,
+        };
+      }
+
+      return {
+        total: total ? `${total}${buttonName}` : buttonName,
+        next,
+        operation,
+      };
+  }
+}
