c428fd9b4db4342ef23518cb1bca212016fda81e
George <71434803+george-swift@users.noreply.github.com>
2021-06-27 15:06:17 +0100

    Add error handling with try catch blocks

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,24 +1,36 @@
 import Big from 'big.js';
 
 export default function operate(numberOne, operation, numberTwo) {
-  const num1 = Big(numberOne);
-  const num2 = Big(numberTwo);
+  let num1;
+  let num2;
+
+  try {
+    num1 = Big(numberOne);
+    num2 = Big(numberTwo);
+  } catch (error) {
+    return 0;
+  }
+
   const format = (bigNumber) => (bigNumber.c.length >= 10
     ? bigNumber.toFixed(8).toString()
     : bigNumber.toString());
 
-  switch (operation) {
-    case '+':
-      return format(num1.plus(num2));
-    case '-':
-      return format(num1.minus(num2));
-    case 'x':
-      return format(num1.times(num2));
-    case '/':
-      return format(num1.div(num2));
-    case '+/-':
-      return format(num1.times(-1));
-    default:
-      return '0';
+  try {
+    switch (operation) {
+      case '+':
+        return format(num1.plus(num2));
+      case '-':
+        return format(num1.minus(num2));
+      case 'x':
+        return format(num1.times(num2));
+      case '/':
+        return format(num1.div(num2));
+      case '+/-':
+        return format(num1.times(-1));
+      default:
+        return '0';
+    }
+  } catch (error) {
+    return 'Error';
   }
 }
