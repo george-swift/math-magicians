c67cc1ab17b5b6b43cb52bb266e7d3e5beb1fcba
George <71434803+george-swift@users.noreply.github.com>
2021-07-01 17:51:00 +0100

    Add navbar component

---
+++ src/components/Nav.js
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import { FaMagic } from 'react-icons/fa';
+import { GoHome } from 'react-icons/go';
+import { IoMdCalculator } from 'react-icons/io';
+import { RiChatQuoteFill } from 'react-icons/ri';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light bg-gradient sticky-top">
+    <div className="container-fluid">
+      <Link
+        to="/"
+        className="navbar-brand"
+      >
+        <span className="me-2">Math Magicians</span>
+        <FaMagic />
+      </Link>
+      <button
+        type="button"
+        className="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div
+        className="collapse navbar-collapse"
+        id="navbarNav"
+      >
+        <div className="navbar-nav ms-auto">
+          <Link
+            to="/"
+            className="nav-link"
+          >
+            <GoHome />
+            <span className="ms-2">Home</span>
+          </Link>
+          <Link
+            to="/calculator"
+            className="nav-link"
+          >
+            <IoMdCalculator />
+            <span className="ms-2">Calculator</span>
+          </Link>
+          <Link
+            to="/quotes"
+            className="nav-link"
+          >
+            <RiChatQuoteFill />
+            <span className="ms-2">Quotes</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
